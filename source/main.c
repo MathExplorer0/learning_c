af8434c3848aebda8d74f7fd241a07d99dad9cfd
MathExplorerGH <mathexplorer@tutamail.com>
2024-04-29 01:54:02 +0100

    adding input

--- source/main.c
+++ source/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int main() {
+    int input;
     int x = 0, y = 1, z = 0;
+    
+    printf("input the value of n: ");
+    scanf("%d", &input);
 
-    for(int i = 0; i < 11; ++i) {
+    for(int i = 0; i < input + 1; ++i) {
         z = x + y;
         y = x;
         x = z;
